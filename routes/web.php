8c0cf484698e064a697adf4d5ebeeecfea0ad2c9
momo33467 <momo334678@gemail.com>
2024-07-07 18:21:49 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    
+
+    // $statment = $database->prepare("INSERT INTO users(First_Name,last_Name,roles,userName,age,shipping_info,password) values('mohammed','mohammed','c','mohammed','1990-05-15','mohammed','mohammed')");
+    // if($statment->execute()){
+    //     return "done";
+    // }else{
+    //     return "none";
+    // }
+
+    return view("login")->with("name",$_GET["name"]);
 });
+
